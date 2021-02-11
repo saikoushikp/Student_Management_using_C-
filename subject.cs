4d503f451b33e55b026c3e2fd094c8fbf851e011
saikoushikp <75295510+saikoushikp@users.noreply.github.com>
2021-02-11 21:02:54 +0530

    Create subject.cs
    Created subject class

---
+++ subject.cs
@@ -0,0 +1,12 @@
+namespace task1
+{        public enum subjects
+        {
+            Telugu,
+            Hindi,
+            English,
+            Maths,
+            Science,
+            Social
+
+        }
+}

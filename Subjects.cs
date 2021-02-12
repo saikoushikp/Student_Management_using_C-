5487fb0ac1fecf8dcf666d353d6189f9340a5bbb
saikoushikp <75295510+saikoushikp@users.noreply.github.com>
2021-02-12 20:10:48 +0530

    Update Subjects.cs

---
+++ Subjects.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_Management_System
+{
+    public enum Subjects
+    {
+        Telugu,
+        Hindi,
+        English,
+        Maths,
+        Science,
+        Social
+
+    }
+}

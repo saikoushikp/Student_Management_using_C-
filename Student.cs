993f548b0830aa3d18a78781c6a11607b3b453af
saikoushikp <75295510+saikoushikp@users.noreply.github.com>
2021-02-15 21:58:37 +0530

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -13,55 +13,5 @@ namespace Student_Management_System
 
         public int[] marks = new int[6];
 
-        
-
-        public Student()
-        {
-            Console.WriteLine("Enter student name:");
-            this.Name = Console.ReadLine();
-            Console.WriteLine("Enter student roll number:");
-            int result = -1;
-            int x = -1;
-            while (x != 0)
-            {
-                result = Convert.ToInt32(Console.ReadLine());
-                x = Services.checkduplicates(result);
-                if (x == 0)
-                    break;
-                Console.WriteLine("Student roll number already exists enter a valid one");
-            }
-            this.rollnumber = result;
-        }
-        public void setMarks()
-        {
-            int cnt = 0;
-            foreach (Subjects s in Enum.GetValues(typeof(Subjects)))
-            {
-                Console.WriteLine("Enter marks for " + s + " Subject");
-                marks[cnt] = Convert.ToInt32(Console.ReadLine());
-                totalMarks = totalMarks + marks[cnt];
-                cnt++;
-            }
-            percentage = totalMarks / (double)6;
-            percentage = (double)System.Math.Round(percentage, 3);
-        }
-
-        public void displayMarks()
-        {
-            int c = -1;
-            foreach (Subjects s in Enum.GetValues(typeof(Subjects)))
-            {
-                Console.WriteLine(s + ":" + marks[c + 1]);
-                c++;
-            }
-            for (int i = 0; i < 30; i++)
-                Console.Write("-");
-            Console.WriteLine();
-            Console.WriteLine("Total Marks :" + totalMarks);
-            Console.WriteLine("Percentage :" + String.Format("{0:0.00}", percentage));
-            for (int i = 0; i < 30; i++)
-                Console.Write("-");
-            Console.WriteLine();
-        }
     }
 }

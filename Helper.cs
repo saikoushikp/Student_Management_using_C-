f2a4ba7fa8ade0d33c0e78a9cef1b9b625825a32
saikoushikp <75295510+saikoushikp@users.noreply.github.com>
2021-02-17 22:31:24 +0530

    Create Helper.cs

---
+++ Helper.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_Management_System
+{
+    class Helper
+    {
+        
+        public void GetSubjectName(int index)
+        {
+            Console.WriteLine("Enter marks for " + (Subjects)index);
+        }
+
+        public void DisplayStudentDetails(Student student,int rollnumber)
+        {
+            
+                    Console.WriteLine("Student Roll Number:" + student.rollnumber);
+                    Console.WriteLine("Student Name:" + student.name);
+                    Console.WriteLine("Student marks\n");
+                    for (int i = 0; i < 20; i++)
+                        Console.Write("-");
+                    Console.WriteLine();
+                    int c = -1;
+                    foreach (Subjects s in Enum.GetValues(typeof(Subjects)))
+                    {
+                        Console.WriteLine(s + ":" + student.marks[c + 1]);
+                        c++;
+                    }
+                    for (int i = 0; i < 30; i++)
+                        Console.Write("-");
+                    Console.WriteLine();
+                    Console.WriteLine("Total Marks :" + student.totalMarks);
+                    Console.WriteLine("Percentage :" + String.Format("{0:0.00}", student.percentage));
+                    for (int i = 0; i < 30; i++)
+                        Console.Write("-");
+                    Console.WriteLine();
+                }
+
+            }
+        }

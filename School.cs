990840e909dc504fe848d9eef374d8c471afd0e8
saikoushikp <75295510+saikoushikp@users.noreply.github.com>
2021-02-17 12:05:09 +0530

    Update School.cs

--- School.cs
+++ School.cs
@@ -6,7 +6,7 @@ namespace Student_Management_System
 {
     public class School
     {
-        public String SchoolName;
+        public String SchoolName { get; set; }
         public List<Student> StudentList = new List<Student>();
     }
 }
